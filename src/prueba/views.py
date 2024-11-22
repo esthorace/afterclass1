b9d67eb6db7e3b928966bf485383dddc78c374c3
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-11-22 19:00:57 -0300

    feat: prueba de views y templates

--- src/prueba/views.py
+++ src/prueba/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    from datetime import datetime
+
+    ahora = datetime.now()
+    nombre = input('Nombre: ')
+    apellido = input('Apellido: ')
+    datos = {
+        'nombre': nombre,
+        'apellido': apellido,
+        'año': f'{ahora.year}',
+    }
+    return render(request, 'prueba/index.html', context=datos)
+
+
+def base(request):
+    return render(request, 'base.html')
